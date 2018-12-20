f102b2e758f4f5d40a39b569fbcd37a205080e66
niquola <niquola@gmail.com>
2018-12-20 23:39:10 +0300

    progress

--- src/fhirpath/core.clj
+++ src/fhirpath/core.clj
@@ -5,7 +5,8 @@
            FHIRPathVisitor
            [org.antlr.v4.runtime CharStreams CommonTokenStream]
            [org.antlr.v4.runtime.tree ParseTreeWalker])
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set]))
 
 
 (defn to-list [x]
@@ -63,8 +64,17 @@
 
     (visitFunctionInvocation [^FHIRPathParser$FunctionInvocationContext ctx]
       (let [[fn-name & params :as call] (first (proxy-super visitChildren ctx))]
-        (if (= fn-name :where)
-          (to-list ['fhirpath.core/fp-where (to-list ['fn ['doc] (ffirst params)])])
+        (cond
+          (contains? #{:ofType} fn-name)
+          (to-list [(symbol (str "fhirpath.core/fp-" (name fn-name)))
+                    (last (last (ffirst params)))])
+
+          (contains? #{:where :select :repeat :exists :all} fn-name)
+          (if-let [lambda  (ffirst params)]
+            (to-list [(symbol (str "fhirpath.core/fp-" (name fn-name)))
+                      (to-list ['fn ['doc] lambda])])
+            (to-list [(symbol (str "fhirpath.core/fp-" (name fn-name)))]))
+          :else
           (to-list (into [(symbol (str "fhirpath.core/fp-" (name fn-name)))] (first params))))))
 
     (visitParamList [^FHIRPathParser$ParamListContext ctx]
@@ -75,9 +85,13 @@
       (proxy-super visitChildren ctx))
 
     ;; (visitImpliesExpression [^FHIRPathParser$ImpliesExpressionContext ctx])
-    ;; (visitInequalityExpression [^FHIRPathParser$InequalityExpressionContext ctx])
     ;; (visitMultiplicativeExpression [^FHIRPathParser$MultiplicativeExpressionContext ctx])
     ;; (visitNullLiteral [^FHIRPathParser$NullLiteralContext ctx])
+
+    (visitInequalityExpression [^FHIRPathParser$InequalityExpressionContext ctx]
+      (to-list (into ['fhirpath.core/fp-ineq
+                      (symbol (.getText (.getChild ctx 1)))]
+                     (proxy-super visitChildren ctx))))
 
     (visitNumberLiteral [^FHIRPathParser$NumberLiteralContext ctx]
       (Float/parseFloat (.getText (.NUMBER ctx))))
@@ -91,14 +105,22 @@
     ;; (visitQualifiedIdentifier [^FHIRPathParser$QualifiedIdentifierContext ctx])
     ;; (visitQuantity [^FHIRPathParser$QuantityContext ctx])
     ;; (visitQuantityLiteral [^FHIRPathParser$QuantityLiteralContext ctx])
+
     (visitStringLiteral [^FHIRPathParser$StringLiteralContext ctx]
       (str/replace (.getText (.STRING ctx))
                    #"(^'|'$)" ""))
-    ;; (visitThisInvocation [^FHIRPathParser$ThisInvocationContext ctx])
+    (visitThisInvocation [^FHIRPathParser$ThisInvocationContext ctx]
+      'identity
+      )
+    
     ;; (visitTimeLiteral [^FHIRPathParser$TimeLiteralContext ctx])
     ;; (visitTypeExpression [^FHIRPathParser$TypeExpressionContext ctx])
     ;; (visitTypeSpecifier [^FHIRPathParser$TypeSpecifierContext ctx])
-    ;; (visitUnionExpression [^FHIRPathParser$UnionExpressionContext ctx])
+
+    (visitUnionExpression [^FHIRPathParser$UnionExpressionContext ctx]
+      (to-list (into ['fhirpath.core/fp-union] (proxy-super visitChildren ctx)))
+      )
+
     ;; (visitUnit [^FHIRPathParser$UnitContext ctx])
 
     ))
@@ -123,7 +145,9 @@
                        (if (not (nil? res))
                          (conj acc res)
                          acc)))) []))
-    (get subj k)))
+    (or (get subj k)
+        (when (= (:resourceType subj) (name k))
+          subj))))
 
 (defn fp-eq [a b]
   (= a b))
@@ -134,8 +158,52 @@
 (defn fp-where [s f]
   (filter f s))
 
+(defn fp-select [s f]
+  (reduce (fn [acc x]
+            (let [res (f x)]
+              (if (sequential? res)
+                (into acc res)
+                (if (nil? res)
+                  acc
+                  (conj acc res)))
+              )) []
+          (if (sequential? s) s [s])))
+
+(defn fp-count [s]
+  (count s))
+
+(defn fp-repeat [s f]
+  (let [init (fp-select s f)]
+    (loop [res init 
+           work init]
+      (let [more (fp-select work f)]
+        (if (and more (empty? more))
+          res
+          (recur (into res more) more))))))
+
+(defn seqy [s]
+  (if (sequential? s) s (if (nil? s) [] [s])))
+
+
+(defn fp-ofType [s tp]
+  (let [v (seqy s)]
+    (filter (fn [x]
+              (cond
+                (= tp :string)  (string? x)
+                (= tp :integer) (integer? x)
+                (= tp :decimal) (number? x)
+                (= tp :boolean) (boolean? x)
+                (= tp :object) (map? x)
+                ))
+            v)))
+
+
+
+
 (defn fp-nth [s n]
-  (get s (int n)))
+  (println (seqy s) (int n)
+           (get (seqy s) (int n)))
+  (get (seqy s) (int n)))
 
 
 (defn compile [expr]
@@ -144,11 +212,87 @@
 (defn fp [expr data]
   ((compile expr) data))
 
-(parse "a.b[0]")
+(defn fp-ineq [op a b]
+  (if (and (number? a) (number? b))
+    (op a b)
+    false))
 
-;; public T visitChildren(RuleNode node) {
-;;                                        System.out.println(node.getClass().getSimpleName() + " " + node.getText());
-;;                                        return super.visitChildren(node);
-;;                                        }
+(defn fp-union [a b]
+  (dedupe
+   (into (or a []) b)))
+
+(defn fp-empty [s]
+  (if (sequential? s)
+    (empty? s)
+    (if (nil? s)
+      true false)))
 
 
+
+(defn fp-not [s]
+  (if (sequential? s)
+    (if (empty? s)
+      []
+      (let [val (and (= (count s) 1) (first s))]
+        (if (boolean? s) (not s)
+            false)))
+    (not s)))
+
+(defn fp-exists [s & [f]]
+  (if f
+    (fp-exists (fp-where s f))
+    (not (empty? (seqy s)))))
+
+(defn fp-all [s f]
+  (every? f (if (sequential? s) s (if (nil? s) [] [s]))))
+
+(defn fp-allTrue [s]
+  (every? #(= true %) (if (sequential? s) s (if (nil? s) [] [s]))))
+
+(defn fp-anyTrue [s]
+  (let [s (seqy s)]
+    (if (empty? s)
+      false
+      (not (nil? (some true? s))))))
+
+(defn fp-allFalse [s]
+  (every? false? (seqy s)))
+
+(defn fp-anyFalse [s]
+  (let [s (seqy s)]
+    (if (empty? s)
+      false
+      (not (nil? (some false? s))))))
+
+(defn fp-subsetOf [s m]
+  (println ">>>>subsetof"
+           (into #{} (seqy s))
+           (into #{} (seqy m)))
+  (clojure.set/subset? (into #{} (seqy s))
+                       (into #{} (seqy m))))
+
+(defn fp-supersetOf [s m]
+  (clojure.set/superset? (into #{} (seqy s))
+                         (into #{} (seqy m))))
+
+
+(defn fp-isDistinct [s]
+  (distinct? s))
+
+(defn fp-distinct [s]
+  (dedupe (seqy s)))
+
+(parse "a.exists(a = 1)")
+(parse "Functions.coll1[0]")
+
+(fp "Functions.coll1"
+    {:resourceType "Functions"
+     :coll1 [{:coll2 [{:attr 1} {:attr 2}]}]})
+
+(do
+  (println "\n\n")
+  (parse "a.coll1.coll2[0]"))
+
+(fp "Functions.coll1[0]"
+    {:resourceType "Functions"
+     :coll1 [{:coll2 [{:attr 1} {:attr 2}]}]})
--- test/fhirpath/core_test.clj
+++ test/fhirpath/core_test.clj
@@ -1,6 +1,8 @@
 (ns fhirpath.core-test
   (:require [fhirpath.core :as sut]
-            [clojure.test :refer :all]))
+            [clj-yaml.core :as yaml]
+            [clojure.test :refer :all]
+            [clojure.java.io :as io]))
 
 
 
@@ -41,3 +43,58 @@
 
   )
 
+
+(defn do-test [path]
+  (let [data (yaml/parse-string (slurp (io/resource path)))]
+    (doseq [t (:tests data)]
+      (println (:desc t))
+      (println "EXPR=>" (:expression t))
+
+      (when (:expression t)
+        (let [res (sut/fp (:expression t) (:subject data))]
+          (is (= (:result t) res)
+              (str (:desc t) " " (:expression t) )))))))
+
+(deftest fhipath-tests
+  (do-test "cases/4.1_literals.yaml")
+
+
+  (is (= [3 4 5]
+         (sut/fp "a.b.where($this > 2)" {:a {:b [1 2 3 4 5]}})))
+
+
+  (is (= [1 2 3 4 5 6]
+         (sut/fp "a.select(b)" {:a [{:b [1 2 3]}
+                                    {:b [4 5 6]}]})))
+
+  (is (= [1 2]
+         (sut/fp "a.select(b)" {:a [{:b 1}
+                                    {:b 2}]})))
+  (is (= [1]
+         (sut/fp "a.select(b)" {:a {:b 1}})))
+  
+  (is (= [{:a {:a 1}} {:a 1} 1]
+         (sut/fp "b.repeat(a)" {:b {:a {:a {:a 1}}}})))
+
+
+  (is (sut/fp "b.all($this > 0)" {:b [1 2 3 3]}))
+  (is (not (sut/fp "b.all($this > 3)" {:b [1 2 3 3]})))
+  (is (sut/fp "b.exists($this > 1)" {:b [1 2 3 3]}))
+  (is (not (sut/fp "b.exists($this > 30)" {:b [1 2 3 3]})))
+
+
+
+  (is (sut/fp "a.supersetOf(b)" {:a [1 2 3] :b [1 2]}))
+  (is (not (sut/fp "b.supersetOf(a)" {:a [1 2 3] :b [1 2]})))
+
+  (is (sut/fp "b.subsetOf(a)" {:a [1 2 3] :b [1 2]}))
+  (is (not (sut/fp "a.subsetOf(b)" {:a [1 2 3] :b [1 2]})))
+
+
+  
+  (do-test "cases/5.2_filtering_and_projection.yaml")
+
+  (do-test "cases/5.1_existence.yaml")
+
+  
+  )
